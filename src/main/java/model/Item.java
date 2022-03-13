915321d49586ab67905bb2a5c58c3a4731fb2a0d
seko0215 <sergei.kobtcev@netcracker.com>
2022-03-13 14:06:52 +0400

    Update

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -2,11 +2,13 @@ package model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@ToString(callSuper = true)
 public final class Item extends Base {
     private UUID partyInteractionId;
     private UUID externalRefId;
--- src/main/java/model/Party.java
+++ src/main/java/model/Party.java
@@ -2,11 +2,13 @@ package model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@ToString(callSuper = true)
 public final class Party extends Base {
     private String startDate;
     private String endDate;
